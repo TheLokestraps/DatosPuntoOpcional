5769811dcf7393cc2447671d4d862cb659f5f3f4
usuario <usuario@DESKTOP-K5SFFJV>
2020-05-15 14:48:53 -0500

    proyecto de servidor con errores (no corre)
    "Si no te gustan los subtitulos entonces te debe gustar doblada" Morgan Freeman

--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -17,11 +17,44 @@ public class Server {
     /**
      * @param args the command line arguments
      */
+//    static ServerSocket self;
+//    static Socket selve;
+//    static int puerto = 5000;
+//    static BufferedReader entrada;    
+        
+    
     public static void main(String[] args) {
         // TODO code application logic here
         ServerSocket self;
         Socket selve;
         int puerto = 5000;
+        BufferedReader entrada;  
+        
+        System.out.println("si corri");
+        try{
+        
+            self = new ServerSocket(puerto);
+            System.out.println("Esperando la conexion");
+            selve = self.accept();
+            System.out.println("se detecto un cliente");
+            entrada = new BufferedReader(new InputStreamReader(selve.getInputStream()));
+            DataOutputStream salida = new DataOutputStream(selve.getOutputStream());       
+            System.out.println("conexion confirmada");
+            String mensajeRecibido = entrada.readLine();
+            System.out.println(mensajeRecibido);
+            salida.writeUTF("Se recibio tu mensaje.");
+            salida.writeUTF("Gracias por conectarte.");
+            System.out.println("Cerrando conexión...");
+            self.close();
+            
+        }catch(IOException e){
+            System.out.println("Errooooor");
+        
+        }
+        
+        
+        
+        
     }
     
     
